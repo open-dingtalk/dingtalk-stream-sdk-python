b81a23b2ae9e6c0b7b8a9dfef838f5ffc5407861
Ke Jie <chzealot@gmail.com>
2023-05-20 10:57:33 +0800

    feat: release to upgrade
    Signed-off-by: Ke Jie <chzealot@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dingtalk-stream',
-    version='0.1.7',
+    version='0.1.8',
     description='A Python library for sending messages to DingTalk chatbot',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
