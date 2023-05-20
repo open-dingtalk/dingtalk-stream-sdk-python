c07a8239affee6ebb39a0f122539f6f14d5be2cb
金喜@DingTalk <chzealot@gmail.com>
2023-05-20 11:28:14 +0800

    feat: release v0.2.0, subscribe specific topics
    * 支持订阅指定的 Topic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dingtalk-stream',
-    version='0.1.13',
+    version='0.2.0',
     description='A Python library for sending messages to DingTalk chatbot',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
