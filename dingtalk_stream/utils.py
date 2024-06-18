b79aebcdfb233940326af66703bb7141b853e782
金喜 <jinxi.kj@alibaba-inc.com>
2024-06-18 18:28:24 +0800

    feat: 支持自定义网络接入点
    Signed-off-by: 金喜 <jinxi.kj@alibaba-inc.com>

--- dingtalk_stream/utils.py
+++ dingtalk_stream/utils.py
@@ -1,3 +1,11 @@
 # -*- coding:utf-8 -*-
 
+import os
+
 DINGTALK_OPENAPI_ENDPOINT = "https://api.dingtalk.com"
+
+def get_dingtalk_endpoint():
+    endpoint_env = os.getenv('DINGTALK_OPENAPI_ENDPOINT')
+    if endpoint_env:
+        return endpoint_env
+    return DINGTALK_OPENAPI_ENDPOINT
